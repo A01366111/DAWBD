31d783796cf9383f005ba2908c4ab80a53732829
JavierIQ <a01366111@itesm.mx>
2019-02-04 16:18:38 -0600

    Lab4- 2 Problemas resueltos

---
+++ lab4.js
@@ -0,0 +1,12 @@
+//Problema 1
+var num1 = prompt("Dame un número mayor a 0");
+var i;
+
+for (i=1;i<=num1;i++){
+    console.log(i+" " +" "+ Math.pow(i,2))
+}
+for (i = 1; i <= num1; i++) {
+    console.log(i+" " + " "+ Math.pow(i, 3))
+}
+
+//Problema 2
