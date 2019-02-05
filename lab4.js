42c3094a6edd54c5edf96e64ebf1e698a16fd526
JavierIQ <a01366111@itesm.mx>
2019-02-05 00:52:23 -0600

    Problema 5

--- lab4.js
+++ lab4.js
@@ -1,4 +1,4 @@
-//Problema 1
+//Problema 1 //Resultado en Consola
 var num1 = prompt("Dame un número mayor a 0");
 var i;
 
@@ -9,7 +9,7 @@ for (i = 1; i <= num1; i++) {
     console.log(i+" " + " "+ Math.pow(i, 3))
 }
 
-//Problema 2
+//Problema 2 Resultado en Consola
 var resultado = prompt("Dame un número del 0 al 40 para adivinar la suma");
     var x1 = Math.floor((Math.random()*20)+1);
     var x2 = Math.floor((Math.random() * 20) + 1);
@@ -18,14 +18,19 @@ var respuesta = x1 + x2;
 
 //Problema 4
 
-//Problema 5
+//Problema 5 Resultado en consola
+var inverso = [1,9,9,7];
+console.log('inverso: ', inverso);
+
+var reversed = inverso.reverse();
+console.log('reversed: ', reversed);
 
 //Problema 6
 alert("¿Te alcanza para un gansito del oxxo?")
 var dinero = prompt("¿Cuánto dinero tienes?");
 
 if (dinero <= 0) {
-    alert("Thank you. Have a nice day!");
+    alert("Tu dinero es negativo checa tu banco, que tengas un buen día");
 } else if (dinero > 12) {
     alert("Si te alcanza! :D Cuesta $12 y tienes $" + dinero + ".");
 } else if (dinero <= 11) {
