c52efacf7575e1749bf5897ccd4a1da0814d45fd
JavierIQ <a01366111@itesm.mx>
2019-03-06 11:50:49 -0600

    lab6

--- js/lab6.js
+++ js/lab6.js
@@ -1,3 +1,5 @@
+  $('.dropdown-trigger').dropdown();
+
   document.addEventListener('DOMContentLoaded', function () {
       var elems = document.querySelectorAll('.dropdown-trigger');
       var instances = M.Dropdown.init(elems, options);
