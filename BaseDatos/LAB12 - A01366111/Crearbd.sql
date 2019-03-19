0132111a2f49e253bb3735c0bd9c0c5b5d96ec74
JavierIQ <a01366111@itesm.mx>
2019-03-19 13:34:59 -0600

    Lab12

--- BaseDatos/LAB12 - A01366111/Crearbd.sql
+++ BaseDatos/LAB12 - A01366111/Crearbd.sql
@@ -11,3 +11,31 @@ sp_helpconstraint Proveedores
 SELECT * 
 FROM Proyectos
 ALTER TABLE Proyectos add Constraint llaveProyectos PRIMARY KEY (Numero)
+SELECT * 
+FROM Alumno
+SELECT * 
+FROM Carrera
+SELECT * 
+FROM Entregan
+SELECT * 
+FROM Materiales
+SELECT * 
+FROM Proveedores
+SELECT * 
+FROM Proyectos
+
+INSERT INTO Entregan Values(0,'AAAA800101',0,'1-jan-02',0)
+Delete from Entregan where Clave = 0 
+
+  ALTER TABLE entregan add constraint cfentreganclave 
+  foreign key (clave) references materiales(clave); 
+  ALTER TABLE entregan add constraint rfcentreganclave 
+  foreign key (rfc) references proveedores(rfc); 
+    ALTER TABLE entregan add constraint Numentreganclave 
+  foreign key (Numero) references Proyectos(Numero); 
+  sp_helpconstraint entregan
+  INSERT INTO entregan values (1000, 'AAAA800101', 5000, GETDATE(), 0); 
+   Delete from Entregan where Clave = 1000 and RFC = 'AAAA800101' and Numero = 5000 and Cantidad = 0
+  SELECT * 
+  FROM Entregan
+
